f0a5b11e948af2920ff0250b02894dbc5cd08e3d
Mad Bernard <madeline.a.bernard@gmail.com>
2015-12-23 13:35:11 -0800

    fixed a bit more of merge in index.html; added semicolons to Decision.js

--- Decision.js
+++ Decision.js
@@ -3,8 +3,8 @@ var Decision = function(title, text) {
 	this.title = title;
 	this.storyText = text;
 	this.choices = [];
-}
+};
 
 Decision.prototype.addChoice = function(decision) {
 	this.choices.push(decision);
-}
+};
